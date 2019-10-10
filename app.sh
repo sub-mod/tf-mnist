0dee1abfc0607ca590009813341dd20e95008cd3
Subin <subin@apache.org>
2019-10-10 19:03:16 -0400

    add CI/CD manifests

--- app.sh
+++ app.sh
@@ -1,6 +1,8 @@
 
 echo "# running app script"
 
+# is empty check is a must if this value is set from a manifest file
+# also always set default values somewhere
 if [[ -z "${STOP_THRESHOLD}" ]]; then
   echo "STOP_THRESHOLD is undefined"
 fi
